8cf964fc0ef142e0b773de2b635da2713a172d06
András Ignéczi <andras.igneczi@doclerholding.com>
2016-04-13 15:52:03 +0200

    - The webservice displays a simple example highchart

--- Webhost/src/main/java/www/WebService.java
+++ Webhost/src/main/java/www/WebService.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by Andras on 08/04/2016.
@@ -14,6 +15,10 @@ public class WebService
 	@RequestMapping( "/" )
 	public String index() throws IOException
 	{
-		return "Hello World!";
+		Scanner lScanner  = new Scanner(getClass().getClassLoader().getResourceAsStream("charts_template.html" ), "UTF-8" );
+		String  lHtml = lScanner.useDelimiter( "\\A" ).next();
+		lScanner.close();
+
+		return lHtml;
 	}
 }
