9ffd3c5a5cca0e4e217f6a93186f1850671de469
András Ignéczi <andras.igneczi@doclerholding.com>
2016-06-29 18:02:34 +0200

    Single runnable jar from the source by maven plugins.

--- DataCollector/src/main/java/PageGuest/WizzAirPageGuest.java
+++ DataCollector/src/main/java/PageGuest/WizzAirPageGuest.java
@@ -17,7 +17,12 @@ import java.io.StringWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.logging.Level;
 import java.util.stream.IntStream;
+
+import static com.teamdev.jxbrowser.chromium.LoggerProvider.getBrowserLogger;
+import static com.teamdev.jxbrowser.chromium.LoggerProvider.getChromiumProcessLogger;
+import static com.teamdev.jxbrowser.chromium.LoggerProvider.getIPCLogger;
 
 /**
  * Created by Andras on 15/03/2016.
@@ -35,6 +40,9 @@ public class WizzAirPageGuest extends WebPageGuest implements Runnable
 	public WizzAirPageGuest()
 	{
 		super( "wizzair" );
+		getBrowserLogger().setLevel( Level.WARNING );
+		getChromiumProcessLogger().setLevel( Level.WARNING );
+		getIPCLogger().setLevel( Level.WARNING );
 		mSearchQueue = new ArrayList<TravelData_INPUT>();
 		mThread = new Thread( this );
 		mThread.setName( "WizzAirThread " + LocalDateTime.now().format( DateTimeFormatter.ISO_LOCAL_DATE_TIME ) );
@@ -57,6 +65,10 @@ public class WizzAirPageGuest extends WebPageGuest implements Runnable
 	{
 		if( mWizzAirPageGuestList != null )
 			return;
+
+		getBrowserLogger().setLevel( Level.WARNING );
+		getChromiumProcessLogger().setLevel( Level.WARNING );
+		getIPCLogger().setLevel( Level.WARNING );
 
 		JTabbedPane lTabbedPane = new JTabbedPane();
 		JFrame frame = new JFrame( "Travel Optimizer - wizzair" );
