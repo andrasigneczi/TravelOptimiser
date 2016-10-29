513b3abdd23c17239dd8132105e09d0d2919da3f
András Ignéczi <andras.igneczi@doclerholding.com>
2016-10-29 08:31:23 +0200

    webhost favorites logger

--- Webhost/src/main/java/Util/Favorites.java
+++ Webhost/src/main/java/Util/Favorites.java
@@ -1,5 +1,7 @@
 package Util;
 
+import org.apache.log4j.Logger;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -14,6 +16,7 @@ import java.util.regex.Pattern;
  */
 public class Favorites
 {
+	private static org.apache.log4j.Logger mLogger = Logger.getLogger(Favorites.class);
 	private ArrayList<String> mTrips = new ArrayList<>();
 	private final static String mFileName = "favourite_flights.txt";
 
@@ -42,12 +45,13 @@ public class Favorites
 		}
 		catch( FileNotFoundException e )
 		{
-			e.printStackTrace();
+			mLogger.info( Util.getTraceInformation( e ));
 			return;
 		}
 		catch( java.util.NoSuchElementException e )
 		{
 			lScanner.close();
+			mLogger.info( Util.getTraceInformation( e ));
 			return;
 		}
 
@@ -70,11 +74,13 @@ public class Favorites
 		}
 		catch( FileNotFoundException e )
 		{
-			e.printStackTrace();
+			mLogger.info( "Faverites saving error!");
+			mLogger.error( Util.getTraceInformation( e ));
 		}
 		catch( UnsupportedEncodingException e )
 		{
-			e.printStackTrace();
+			mLogger.info( "Faverites saving error!");
+			mLogger.error( Util.getTraceInformation( e ));
 		}
 	}
 
