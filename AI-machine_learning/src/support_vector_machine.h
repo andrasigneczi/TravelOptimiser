72c985caad40a26dce9a07bbe6aeeb14e8d32ee9
Andras <andras.igneczi@gmail.com>
2018-05-05 14:40:20 +0200

    libsvm for support vector machine

---
+++ AI-machine_learning/src/support_vector_machine.h
@@ -0,0 +1,11 @@
+#ifndef __SUPPORT_VECTOR_MACHINE_H__
+#define __SUPPORT_VECTOR_MACHINE_H__
+
+
+class support_vector_machine
+{
+public:
+    support_vector_machine();
+};
+
+#endif // __SUPPORT_VECTOR_MACHINE_H__
