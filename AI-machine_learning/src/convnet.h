3e1b3aecf9b9ad2c67b7817252946d2f8beb753b
Andras <andras.igneczi@gmail.com>
2019-01-01 18:06:57 +0100

    x

--- AI-machine_learning/src/convnet.h
+++ AI-machine_learning/src/convnet.h
@@ -1,5 +1,6 @@
 #ifndef CONVNET_H
 #define CONVNET_H
+#include "convnet/forward_backward_if.h"
 
 /*
  * Layer types
@@ -36,6 +37,27 @@ class ConvNet
 {
 public:
     ConvNet();
+    ConvNet(std::prefix);
+    ConvNet& operator<<(ForwardBackwardIF* obj) { mLayers.push_back(obj); return *this; }
+
+    void miniBatchGradientDescent( long long epoch, size_t batchSize, double learning_rate,
+                                                    double beta, double beta1, double beta2,
+                                                    double epsilon );
+    arma::mat4D forward(arma::mat4D X);
+    void backward(arma::mat4D AL, arma::mat Y);
+
+    bool saveState(std::string prefix);
+    bool loadState(std::string prefix);
+    void continueMinibatch(long long epoch);
+
+    arma::mat predict(const arma::mat& X, double* cost = 0, bool ignoreFeatureScaling = false);
+    double accuracy(double* cost = 0);
+
+    double compute_cost_with_regularization(const arma::mat& A3, const arma::mat& Y, ActivationFunction af);
+    double compute_cost(const arma::mat& AL, const arma::mat& Y, ActivationFunction af = SIGMOID);
+
+private:
+    std::vector<ForwardBackwardIF*> mLayers;
 };
 
 #endif // CONVNET_H
